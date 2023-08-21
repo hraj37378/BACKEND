8bec9e784447760130b5bc1d1955dfaa37968359
hraj37378 <hraj37378@gmail.com>
2023-08-21 20:01:44 +0530

    Version 2

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ app.use(express.json());
 // Available Routes
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
-
+app.use(express.static(__dirname+"/public"));
+app.use("/public",express.static(__dirname+"/public"));
+app.get("/",(req,res)=>{
+  res.render("index");
+})
  
 
 
